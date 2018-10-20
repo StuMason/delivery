317ca2da2adf2cf026209cd4d0545d30e2089ef9
Stu Mason <stuart.mason@paddle.com>
2018-10-20 09:50:51 +0100

    permissions

---
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        // create permissions
+        Permission::create(['name' => 'edit order']);
+        Permission::create(['name' => 'delete order']);
+        Permission::create(['name' => 'create order']);
+
+        // create roles and assign created permissions
+        $role = Role::create(['name' => 'unverified']);
+
+        $role = Role::create(['name' => 'restaurateur']);
+        $role->givePermissionTo(['edit order']);
+
+        $role = Role::create(['name' => 'driver']);
+        $role->givePermissionTo(['edit order']);
+
+        $role = Role::create(['name' => 'user']);
+        $role->givePermissionTo(['create order']);
+
+        $role = Role::create(['name' => 'super-admin']);
+        $role->givePermissionTo(Permission::all());
+    }
+}
